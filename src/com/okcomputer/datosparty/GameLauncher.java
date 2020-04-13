e25d482cd97c924fdc01d329debdcc140a524f94
JoeA42 <joe.alvarez42@gmail.com>
2020-04-13 13:51:34 -0600

    refactored GameLaunch into GameLauncher

--- src/com/okcomputer/datosparty/GameLauncher.java
+++ src/com/okcomputer/datosparty/GameLauncher.java
@@ -2,7 +2,7 @@ package com.okcomputer.datosparty;
 
 import com.okcomputer.datosparty.display.Display;
 
-public class GameLaunch {
+public class GameLauncher {
     public static void main(String[] args){
         GameLoop gameLoop = new GameLoop("Datos Party",800,600);
         gameLoop.start();
