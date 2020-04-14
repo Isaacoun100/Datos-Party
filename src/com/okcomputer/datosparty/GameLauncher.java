c4788bd9fa7df2a6a14f3f1123879d24b2234f97
Isaac <Isaacoun100@estudiantec.cr>
2020-04-13 19:38:17 -0600

    **Neccesary cleaning**

--- src/com/okcomputer/datosparty/GameLauncher.java
+++ src/com/okcomputer/datosparty/GameLauncher.java
@@ -3,6 +3,7 @@ package com.okcomputer.datosparty;
 import com.okcomputer.datosparty.display.Display;
 
 public class GameLauncher {
+
     public static void main(String[] args){
         GameLoop gameLoop = new GameLoop("Datos Party",800,600);
         gameLoop.start();
