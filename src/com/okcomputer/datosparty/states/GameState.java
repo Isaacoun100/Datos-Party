023f8b7a7b3b417fbe7f78d166bb7d906925f06a
JoeA42 <joe.alvarez42@gmail.com>
2020-04-14 13:50:56 -0600

    Added Abstract State class for setting multiple States // Added TitleScreenState class to render initial menu in its own state // Added GameState which will be the State for the "main game" // Added MainMenuState for Main Menu

---
+++ src/com/okcomputer/datosparty/states/GameState.java
@@ -0,0 +1,20 @@
+package com.okcomputer.datosparty.states;
+
+import java.awt.*;
+
+public class GameState extends State{
+
+    public GameState(){
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
