adf9154d28bbe93ac0818de050975e729a1f81e8
Isaac <Isaacoun100@estudiantec.cr>
2020-04-13 19:01:39 -0600

    **Neccesary cleaning**

--- src/com/okcomputer/datosparty/GameLauncher.java
+++ src/com/okcomputer/datosparty/GameLauncher.java
@@ -1,1 +1,18 @@
+package com.okcomputer.datosparty;
 
+import com.okcomputer.datosparty.boardWindow.BoardTitle;
+
+public class GameLauncher {
+
+    public GameLauncher(){
+
+        new BoardTitle();
+
+    }
+
+    public static void main(String[] args){
+
+        new GameLauncher();
+
+    }
+}
