03b982e852409afdb9af83c9168cff526e85539f
Isaac <Isaacoun100@estudiantec.cr>
2020-04-13 18:58:57 -0600

    [Minimal change]

---
+++ src/com/okcomputer/datosparty/boardWindow/BoardTitle.java
@@ -0,0 +1,24 @@
+package com.okcomputer.datosparty.boardWindow;
+
+import javax.swing.*;
+
+public class BoardTitle extends JFrame {
+
+    public BoardTitle(){
+
+        setTitle("Datos Party");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setContentPane(new BoardPanel(600,600));
+        setResizable(false);
+
+        pack();
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+
+    }
+
+}
