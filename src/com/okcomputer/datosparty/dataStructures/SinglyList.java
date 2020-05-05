45ad5a0a74b81c3877b09c10bd8b8d54605f8675
GabrielVL <gavalo.99@gmail.com>
2020-05-04 19:13:53 -0600

    [Fixed adding LinkedList] SinglyList and DoublyList now adds lists with index correctly

--- src/com/okcomputer/datosparty/dataStructures/SinglyList.java
+++ src/com/okcomputer/datosparty/dataStructures/SinglyList.java
@@ -53,12 +53,49 @@ public class SinglyList<T> extends LinkedList<T>{
 
     @Override
     public void add(Node<T> node, int index) {
-        insert(node, index);
+        if (index >= getLength()) {
+            System.out.println("Index out of range");
+        } else if (this.head == null) {
+            if (node.getId().equals("Singly")) {
+                this.head = (SinglyNode<T>) node;
+            } else {
+                System.out.println("For list to remain singly, a singly node or list should be added");
+            }
+        } else {
+            Node<T> nodeNext = getNodeByIndex(index);
+            node.setNext(nodeNext);
+            if (index > 0) {
+                Node<T> nodePrevious = getNodeByIndex(--index);
+                nodePrevious.setNext(node);
+            } else {
+                System.out.println("please add a class other than Node or List to the head");
+            }
+        }
     }
 
     @Override
     public void add(LinkedList<T> list, int index) {
-        insert(list.getHead(), index);
+        Node<T> listHead = list.getHead();
+        if (index >= getLength()) {
+            System.out.println("Index out of range");
+        } else if (this.head == null) {
+            if (listHead.getId().equals("Singly")) {
+                this.head = (SinglyNode<T>) listHead;
+            } else {
+                System.out.println("For list to remain singly, a singly node or list should be added");
+            }
+        } else {
+            Node<T> nodeNext = getNodeByIndex(index);
+            listHead.setNext(nodeNext);
+            if (index > 0) {
+                Node<T> nodePrevious = getNodeByIndex(--index);
+                Node<T> lastListNode = list.getLast();
+                nodePrevious.setNext(listHead);
+                lastListNode.setNext(nodeNext);
+            } else {
+                System.out.println("please add a class other than Node or List to the head");
+            }
+        }
     }
 
     @Override
@@ -74,28 +111,6 @@ public class SinglyList<T> extends LinkedList<T>{
             Node<T> lastNode = getLast();
             // Insert the newNode at last node
             lastNode.setNext(newNode);
-        }
-    }
-
-    @Override
-    protected void insert(Node<T> newNode, int index) {
-        if (index >= getLength()) {
-            System.out.println("Index out of range");
-        } else if (this.head == null) {
-            if (newNode.getId().equals("Singly")) {
-                this.head = (SinglyNode<T>) newNode;
-            } else {
-                System.out.println("For list to remain singly, a singly node or list should be added");
-            }
-        } else {
-            Node<T> nodeNext = getNodeByIndex(index);
-            newNode.setNext(nodeNext);
-            if (index > 0) {
-                Node<T> nodePrevious = getNodeByIndex(--index);
-                nodePrevious.setNext(newNode);
-            } else {
-                System.out.println("please add a class other than Node or List to the head");
-            }
         }
     }
 
