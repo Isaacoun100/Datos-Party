1e0f10df8fd29135ec05e15956cf0a8995b5f164
Isaac <Isaacoun100@estudiantec.cr>
2020-05-09 19:12:07 -0600

    [BoardState and EndgameState created] These two classes are the states that will run when called, they now contain nothing but some random sprite sheets

---
+++ src/com/okcomputer/datosparty/states/BoardState.java
@@ -0,0 +1,26 @@
+package com.okcomputer.datosparty.states;
+
+import com.okcomputer.datosparty.Handler;
+
+import java.awt.*;
+
+public class BoardState extends State {
+    /**
+     * Main Constructor for the State
+     *
+     * @param handler handler element is the only parameter
+     */
+    public BoardState(Handler handler) {
+        super(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
---
+++ src/com/okcomputer/datosparty/states/EndgameState.java
@@ -0,0 +1,50 @@
+package com.okcomputer.datosparty.states;
+
+import com.okcomputer.datosparty.Handler;
+import com.okcomputer.datosparty.gfx.Assets;
+import com.okcomputer.datosparty.userInterface.UIManager;
+import com.okcomputer.datosparty.userInterface.ClickListener;
+import com.okcomputer.datosparty.userInterface.UIImageButton;
+
+import java.awt.*;
+
+public class EndgameState extends State {
+
+
+    private UIManager uiManager;
+
+    /**
+     * Main Constructor for the State
+     *
+     * @param handler handler element is the only parameter
+     */
+    public EndgameState(Handler handler) {
+        super(handler);
+
+        uiManager = handler.getUIManager();
+        handler.getMouseManager().setUiManager(uiManager);
+
+        uiManager.addObject(new UIImageButton(328, 400, 3*3, 3, Assets.playButton, new ClickListener() {
+            @Override
+            public void onClick() {
+                State.setState(handler.getGameLoop().gameState);
+            }
+        }));
+
+
+    }
+
+    @Override
+    public void tick() {
+        uiManager.tick();
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        uiManager.render(g);
+        g.drawImage(Assets.notOKComputerIcon[0] , 82,100,null);
+        g.drawImage(Assets.notOKComputer[0] , 40,130,null);
+
+    }
+}
