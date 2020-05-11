af8907af1c582797a0637d87a4484b5691e6ce54
Isaac <Isaacoun100@estudiantec.cr>
2020-05-10 22:29:28 -0600

    [BoardUI created] This class is an interface that separates all handles from the one used on the BoardState

---
+++ src/com/okcomputer/datosparty/userInterface/BoardUI.java
@@ -0,0 +1,9 @@
+package com.okcomputer.datosparty.userInterface;
+
+import com.okcomputer.datosparty.Handler;
+
+public class BoardUI extends UIManager{
+    public BoardUI(Handler handler) {
+        super(handler);
+    }
+}
