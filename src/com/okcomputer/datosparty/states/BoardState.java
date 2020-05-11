1d31fac8c393be969a119c982b3394bdeb38afb9
Isaac <Isaacoun100@estudiantec.cr>
2020-05-10 22:42:59 -0600

    [Throw a dice] BoardState now contains a new button to simulate a dice throw

--- src/com/okcomputer/datosparty/states/BoardState.java
+++ src/com/okcomputer/datosparty/states/BoardState.java
@@ -3,9 +3,7 @@ package com.okcomputer.datosparty.states;
 import com.okcomputer.datosparty.Handler;
 import com.okcomputer.datosparty.gfx.Assets;
 import com.okcomputer.datosparty.random.Dice;
-import com.okcomputer.datosparty.userInterface.ClickListener;
-import com.okcomputer.datosparty.userInterface.UIImageButton;
-import com.okcomputer.datosparty.userInterface.UIManager;
+import com.okcomputer.datosparty.userInterface.*;
 
 import java.awt.*;
 
@@ -13,29 +11,24 @@ public class BoardState extends State {
 
     private UIManager uiManager;
 
-    /**
-     * Main Constructor for the State
-     *
-     * @param handler handler element is the only parameter
-     */
     public BoardState(Handler handler) {
         super(handler);
 
-        uiManager = handler.getUIManager();
-/*
-        handler.getMouseManager().setUiManager(uiManager);
+        uiManager = new BoardUI(handler);
 
-        uiManager.addObject(new UIImageButton(20, 25, 4, 4, Assets.diceButton, new ClickListener() {
+        uiManager.addObject(new UIImageButton(1, 1, 4, 4, Assets.diceButton, new ClickListener() {
             @Override
             public void onClick() {
                 System.out.println(Dice.roll());
-            }
-        }));
-*/
-    }
+                }
+            }));
+
+        }
+
 
     @Override
     public void tick() {
+        handler.getMouseManager().setUiManager(uiManager);
         uiManager.tick();
     }
 
