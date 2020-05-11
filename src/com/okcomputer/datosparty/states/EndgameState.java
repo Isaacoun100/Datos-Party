32c9cc45b8d9e7687cf2b0c838e19bede6546ba6
Isaac <Isaacoun100@estudiantec.cr>
2020-05-10 22:43:52 -0600

    [Endgame to Credits] Now you can go from Endgame to Credits using a button

--- src/com/okcomputer/datosparty/states/EndgameState.java
+++ src/com/okcomputer/datosparty/states/EndgameState.java
@@ -2,41 +2,29 @@ package com.okcomputer.datosparty.states;
 
 import com.okcomputer.datosparty.Handler;
 import com.okcomputer.datosparty.gfx.Assets;
-import com.okcomputer.datosparty.userInterface.UIManager;
-import com.okcomputer.datosparty.userInterface.ClickListener;
-import com.okcomputer.datosparty.userInterface.UIImageButton;
+import com.okcomputer.datosparty.userInterface.*;
 
 import java.awt.*;
 
 public class EndgameState extends State {
 
+    private final UIManager uiManager;
 
-    private UIManager uiManager;
 
-    /**
-     * Main Constructor for the State
-     *
-     * @param handler handler element is the only parameter
-     */
     public EndgameState(Handler handler) {
         super(handler);
 
-        uiManager = handler.getUIManager();
-/*
-        uiManager.addObject(new UIImageButton(17, 29, 3*5, 3, Assets.creditsButton, new ClickListener() {
-            @Override
-            public void onClick() {
-                State.setState(handler.getGameLoop().creditsState);
-            }
-        }));
+        uiManager = new MainMenuUI(handler);
+
+        uiManager.addObject(new UIImageButton(18, 32, 7*2, 2*2, Assets.creditsButton,
+                () -> State.setState(handler.getGameLoop().creditsState)));
 
 
- */
     }
     @Override
     public void tick() {
+        handler.getMouseManager().setUiManager(uiManager);
         uiManager.tick();
-
     }
 
     @Override
