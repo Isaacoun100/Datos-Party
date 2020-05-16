1cb221f52a63c54c8df96b685b2a662efefca7c1
Isaac <Isaacoun100@estudiantec.cr>
2020-05-15 21:07:40 -0600

    [More actions added] Now, in order to interact with the music Thread you can use the MusicPlayer class the will contain actions like stop, play, add a song or clip, turn the volume of the clip or song up or down

--- src/com/okcomputer/datosparty/music/MusicPlayer.java
+++ src/com/okcomputer/datosparty/music/MusicPlayer.java
@@ -1,11 +1,12 @@
 package com.okcomputer.datosparty.music;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
+import javax.sound.sampled.*;
 import java.io.File;
 
 public class MusicPlayer {
+
+    private static double gameMusicLevel = 1.0, effectMusicLevel = 1.0;
+    private static Clip clip, track;
 
     public static void playSong(String musicLocation, String songName){
 
@@ -16,11 +17,12 @@ public class MusicPlayer {
             if (musicPath.exists()){
 
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInput);
-                clip.start();
+                track = AudioSystem.getClip();
+                track.open(audioInput);
+                track.start();
 
                 System.out.println(songName);
+
 
             }
 
@@ -31,5 +33,106 @@ public class MusicPlayer {
         catch(Exception ex){ ex.printStackTrace();}
 
     }
+
+    public static void playClip(String musicLocation, String songName){
+
+        try{
+            File clipPath = new File(musicLocation);
+
+            if (clipPath.exists()){
+
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(clipPath);
+                clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+
+                System.out.println(songName);
+
+
+            }
+
+            else{ System.out.println("File does not exist"); }
+
+        }
+
+        catch(Exception ex){ ex.printStackTrace();}
+
+    }
+
+    public static void musicVolumeUp(){
+
+        if (gameMusicLevel>=1.0){ System.out.println("Max Volume Reached"); }
+
+        else{
+
+            FloatControl gameControl = (FloatControl) track.getControl(FloatControl.Type.MASTER_GAIN);
+            gameMusicLevel+=0.05;
+            float dB = (float) (Math.log(gameMusicLevel) / Math.log(10.0) * 20.0);
+            gameControl.setValue(dB);
+
+        }
+
+
+    }
+
+    public static void musicVolumeDown(){
+
+        if (gameMusicLevel<=0.0) { System.out.println("Min Volume Reached");}
+
+        else{
+
+            FloatControl gameControl = (FloatControl) track.getControl(FloatControl.Type.MASTER_GAIN);
+            gameMusicLevel-=0.05;
+            float dB = (float) (Math.log(gameMusicLevel) / Math.log(10.0) * 20.0);
+            gameControl.setValue(dB);
+
+        }
+
+
+    }
+
+    public static void clipVolumeUp(){
+
+        if (effectMusicLevel>=1.0){ System.out.println("Max Volume Reached"); }
+
+        else{
+
+            FloatControl effectControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            effectMusicLevel+=0.05;
+            float dB = (float) (Math.log(effectMusicLevel) / Math.log(10.0) * 20.0);
+            effectControl.setValue(dB);
+
+        }
+
+
+    }
+
+    public static void clipVolumeDown(){
+
+        if (effectMusicLevel<=0.0) {
+            System.out.println("Min Volume Reached");
+        }
+
+        else{
+
+            FloatControl effectControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            effectMusicLevel-=0.05;
+            float dB = (float) (Math.log(effectMusicLevel) / Math.log(10.0) * 20.0);
+            effectControl.setValue(dB);
+
+        }
+
+
+    }
+
+    public static void stopSong(){
+        track.stop();
+    }
+
+    public static void resumeSong(){ track.start();}
+
+    public static long songLength(){ return track.getMicrosecondLength();}
+
+
 
 }
