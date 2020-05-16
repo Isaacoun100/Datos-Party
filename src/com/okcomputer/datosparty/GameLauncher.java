23101d8f863f78969b7d2eb8d6560c1f589224fe
Isaac <Isaacoun100@estudiantec.cr>
2020-05-15 21:24:32 -0600

    [Music Thread] The music thread runs from here in order to run independently.

--- src/com/okcomputer/datosparty/GameLauncher.java
+++ src/com/okcomputer/datosparty/GameLauncher.java
@@ -1,23 +1,16 @@
 package com.okcomputer.datosparty;
 
-import com.okcomputer.datosparty.display.Display;
-
-import java.awt.*;
+import com.okcomputer.datosparty.music.LoopMusic;
 
 public class GameLauncher {
 
-
-
-
     public static void main(String[] args){
 
-        int screenHeight;
-        int screenWidth;
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        screenHeight = screenSize.height;
-        screenWidth = screenSize.width;
         GameLoop gameLoop = new GameLoop("Datos Party",800,608);
         gameLoop.start();
+
+        LoopMusic backgroundMusic = new LoopMusic();
+        backgroundMusic.start();
 
     }
 }
