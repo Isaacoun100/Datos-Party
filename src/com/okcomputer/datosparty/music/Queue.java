59791810d420a95be48cb6350b0a98ec3ef22073
Isaac <Isaacoun100@estudiantec.cr>
2020-05-15 21:09:26 -0600

    [Queue added] This class will contain all the songs that will be displayed in the LoopMusic

--- src/com/okcomputer/datosparty/music/Queue.java
+++ src/com/okcomputer/datosparty/music/Queue.java
@@ -1,4 +1,19 @@
 package com.okcomputer.datosparty.music;
 
+import com.okcomputer.datosparty.dataStructures.SinglyList;
+
 public class Queue {
+
+    public String[] songList;
+    static SinglyList<String[]> queue = new SinglyList<>();
+
+    public String[] plazaMii;
+
+    public static void songList(){
+
+        queue.add(new String[]{"res/audio/background/Nintendo Wii - Mii Channel Theme.wav", "Mii Channel"});
+        queue.add(new String[]{"res/audio/background/Nintendo 3DS Music - StreetPass Mii Plaza Theme 1.wav","Mii Plaza"});
+
+    }
+
 }
