9460f00b72234f50c12326b217258af1b0cfaa54
Isaac <Isaacoun100@estudiantec.cr>
2020-05-15 21:19:03 -0600

    [Volume Control] If you access to OptionsState you can now turn game or effects volume up or down.

--- src/com/okcomputer/datosparty/states/OptionsState.java
+++ src/com/okcomputer/datosparty/states/OptionsState.java
@@ -2,10 +2,9 @@ package com.okcomputer.datosparty.states;
 
 import com.okcomputer.datosparty.Handler;
 import com.okcomputer.datosparty.gfx.Assets;
-import com.okcomputer.datosparty.userInterface.OptionsUI;
-import com.okcomputer.datosparty.userInterface.UIImage;
-import com.okcomputer.datosparty.userInterface.UIImageButton;
-import com.okcomputer.datosparty.userInterface.UIManager;
+import com.okcomputer.datosparty.music.MusicPlayer;
+import com.okcomputer.datosparty.music.SoundEffect;
+import com.okcomputer.datosparty.userInterface.*;
 
 import java.awt.*;
 
@@ -14,14 +13,44 @@ public class OptionsState extends State{
 
     public OptionsState(Handler handler){
         super(handler);
+
         uiManager = new OptionsUI(handler);
 
-        uiManager.addObject(new UIImage(20,10,5*2,5*2, Assets.notOKComputerIcon));
-        uiManager.addObject(new UIImage(16,19,2*9, 2, Assets.notOKComputer));
-        uiManager.addObject(new UIImage(7,26,9*4, 4, Assets.nothingHereMessage));
+        uiManager.addObject(new UIImageButton(44, 3, 3, 3, Assets.volumeUp, new ClickListener() {
+            @Override
+            public void onClick() {
+                SoundEffect.PopSound();
+                MusicPlayer.musicVolumeUp();
+            }}));
+
+        uiManager.addObject(new UIImageButton(3, 3, 3, 3, Assets.volumeDown, new ClickListener() {
+            @Override
+            public void onClick() {
+                SoundEffect.PopSound();
+                MusicPlayer.musicVolumeDown();
+            }}));
+
+        uiManager.addObject(new SingleUIImage(18, 3, 7*2, 2*2, Assets.gameMusic));
+
+        uiManager.addObject(new UIImageButton(44, 11, 3, 3, Assets.volumeUp, new ClickListener() {
+            @Override
+            public void onClick() {
+                SoundEffect.PopSound();
+                MusicPlayer.clipVolumeUp();
+            }}));
+
+        uiManager.addObject(new UIImageButton(3, 11, 3, 3, Assets.volumeDown, new ClickListener() {
+            @Override
+            public void onClick() {
+                SoundEffect.PopSound();
+                MusicPlayer.clipVolumeDown();
+            }}));
+
+        uiManager.addObject(new SingleUIImage(18, 11, 7*2, 2*2, Assets.soundEffects));
 
         uiManager.addObject(new UIImageButton(20, 30, 3*3, 3, Assets.backButton,
                 () -> State.setState(handler.getGameLoop().mainMenuState)));
+
     }
 
     @Override
