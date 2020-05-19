35d647333479f11c801b7345b05fde153e842080
Isaac <Isaacoun100@estudiantec.cr>
2020-05-18 21:18:25 -0600

    [Players added] Player init created

--- src/com/okcomputer/datosparty/states/SelectPlayerState.java
+++ src/com/okcomputer/datosparty/states/SelectPlayerState.java
@@ -2,24 +2,29 @@ package com.okcomputer.datosparty.states;
 
 import com.okcomputer.datosparty.Handler;
 import com.okcomputer.datosparty.gfx.Assets;
+import com.okcomputer.datosparty.logic.Round;
 import com.okcomputer.datosparty.userInterface.*;
 
 import java.awt.*;
 
+
 public class SelectPlayerState extends State {
 
     private final UIManager uiManager;
+    public int player;
 
     public SelectPlayerState(Handler handler) {
         super(handler);
 
         uiManager = new SelectPlayerUI(handler);
-
+        Round.roundInit();
 
         uiManager.addObject(new UIImageButton(1, 1, 7*2, 2*2, Assets.player2Button, new ClickListener() {
             @Override
             public void onClick() {
-                System.out.println("2");
+
+                Round.playerLoop(2);
+                Round.returnPlayers();
                 State.setState(handler.getGameLoop().boardState);
             }
         }));
@@ -27,7 +32,8 @@ public class SelectPlayerState extends State {
         uiManager.addObject(new UIImageButton(35, 1, 7*2, 2*2, Assets.player3Button, new ClickListener() {
             @Override
             public void onClick() {
-                System.out.println("3");
+                Round.playerLoop(3);
+                Round.returnPlayers();
                 State.setState(handler.getGameLoop().boardState);
             }
         }));
@@ -35,7 +41,8 @@ public class SelectPlayerState extends State {
         uiManager.addObject(new UIImageButton(20, 30, 7*2, 2*2, Assets.player4Button, new ClickListener() {
             @Override
             public void onClick() {
-                System.out.println("4");
+                Round.playerLoop(4);
+                Round.returnPlayers();
                 State.setState(handler.getGameLoop().boardState);
             }
         }));
