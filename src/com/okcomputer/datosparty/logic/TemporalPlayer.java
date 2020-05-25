3e798dcc57df765be9c3fc73519abec869201291
Isaac <Isaacoun100@estudiantec.cr>
2020-05-24 22:20:23 -0600

    [final int added] Now, when the player is created it should always contain the diceValue

--- src/com/okcomputer/datosparty/logic/TemporalPlayer.java
+++ src/com/okcomputer/datosparty/logic/TemporalPlayer.java
@@ -3,21 +3,18 @@ package com.okcomputer.datosparty.logic;
 public class TemporalPlayer {
 
     private final int id;
-    private int diceValue;
+    private final int diceValue;
 
     public int getDiceValue() {
         return diceValue;
     }
 
-    public void setDiceValue(int diceValue) {
-        this.diceValue = diceValue;
-    }
-
-    public TemporalPlayer(int id) {
-        this.id = id;
-    }
-
     public int getId() {
         return id;
+    }
+
+    public TemporalPlayer(int id, int diceValue) {
+        this.id = id;
+        this.diceValue = diceValue;
     }
 }
