831869fc0cf53215472ca7849610e3f575368b5c
Isaac <Isaacoun100@estudiantec.cr>
2020-05-25 14:32:33 -0600

    [Data type TemporalPlayer] int to String in the id

--- src/com/okcomputer/datosparty/logic/TemporalPlayer.java
+++ src/com/okcomputer/datosparty/logic/TemporalPlayer.java
@@ -2,18 +2,18 @@ package com.okcomputer.datosparty.logic;
 
 public class TemporalPlayer {
 
-    private final int id;
+    private final String id;
     private final int diceValue;
 
     public int getDiceValue() {
         return diceValue;
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public TemporalPlayer(int id, int diceValue) {
+    public TemporalPlayer(String id, int diceValue) {
         this.id = id;
         this.diceValue = diceValue;
     }
