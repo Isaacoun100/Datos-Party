df3588c3cf092a5014c4ff933565c95064cf6827
Isaac <Isaacoun100@estudiantec.cr>
2020-05-25 22:30:12 -0600

    [New method added] displayPlayer, compareDice, lockDice and order were created

--- src/com/itcr/ce/datosparty/logic/DefineOrder.java
+++ src/com/itcr/ce/datosparty/logic/DefineOrder.java
@@ -5,61 +5,107 @@ import com.itcr.ce.datosparty.dataStructures.SinglyNode;
 
 public class DefineOrder {
 
-    private static SinglyList<TemporalPlayer> diceThrowList;
+    private static SinglyList<TemporalPlayer> TemporalPlayerList;
+    private static SinglyList<Integer> diceThrowList;
     TemporalPlayer newPlayer;
 
     public static void initTemporal(){
+        TemporalPlayerList = new SinglyList<>();
+    }
+
+    public static void initDice(){
         diceThrowList = new SinglyList<>();
     }
 
     public void addNewTemporal(TemporalPlayer newTemporal){
-        diceThrowList.add(newTemporal);
+        TemporalPlayerList.add(newTemporal);
     }
 
-    public SinglyList<TemporalPlayer> recursiveAdd(int playerCount){
+    public void recursiveAdd(int playerCount){
+        this.initDice();
 
         while(playerCount>0){
-            newPlayer = new TemporalPlayer("Player " + playerCount, Dice.roll());
-            System.out.println("[ "+ newPlayer.getId() +" , "+ newPlayer.getDiceValue() +" ]");
+            newPlayer = new TemporalPlayer("Player " + playerCount, lockDice());
             this.addNewTemporal(newPlayer);
             playerCount--;
         }
-        return diceThrowList;
+
+        this.displayPlayer(TemporalPlayerList);
 
     }
 
-    private static void swap(SinglyNode<Player> node1, SinglyNode<Player> node2) {
-        Player data1 = node1.getData();
-        Player data2 =  node2.getData();
-        node1.setData(data2);
-        node2.setData(data1);
-    }
+    public void displayPlayer(SinglyList<TemporalPlayer> displayList){
 
-    public SinglyList<TemporalPlayer> searchDraw(){
+        SinglyNode<TemporalPlayer> temporal = displayList.getHead();
 
-        SinglyNode<TemporalPlayer> temp = diceThrowList.getHead();
-        SinglyNode<TemporalPlayer> search = temp;
-        SinglyList<TemporalPlayer> drawPlayers = new SinglyList<>();
-
-        while(temp!=null){
-            while(search!=null){
-                if(search!=temp){
-                    if(search.getData().getDiceValue()==temp.getData().getDiceValue()){
-                        drawPlayers.add(temp.getData());
-                    }
-                }
-                search=search.getNext();
-            }
-            temp=temp.getNext();
-            search= diceThrowList.getHead();
+        while(temporal!=null){
+            System.out.println("[ "+ temporal.getData().getId() +" , "+ temporal.getData().getDiceValue() +" ]");
+            temporal=temporal.getNext();
         }
 
-        return drawPlayers;
+    }
+
+    public boolean compareDice(int dice){
+
+        SinglyNode<Integer> value = diceThrowList.getHead();
+
+        while (value != null) {
+
+            if (dice == value.getData()) {
+                return true;
+            }
+            value = value.getNext();
+        }
+
+        return false;
 
     }
 
-    public void newOrder(SinglyList<TemporalPlayer> oldList){
+    public int lockDice(){
 
+        int dice = Dice.roll();
+
+        while (compareDice(dice)){
+            dice = Dice.roll();
+            System.out.println("Why do they always send the poor");
+        }
+
+        diceThrowList.add(dice);
+        return dice;
+
+    }
+
+    public void order() {
+        int count=0;
+        SinglyNode<TemporalPlayer> before = TemporalPlayerList.getHead();
+        SinglyNode<TemporalPlayer> after = before;
+
+        while (count< TemporalPlayerList.getLength()){
+            while(before!=null){
+                after=before.getNext();
+                while (after!=null){
+                    if (before.getData().getDiceValue()<after.getData().getDiceValue()){
+                        this.swap(before,after);
+                    }
+                    after=after.getNext();
+                }
+                before=before.getNext();
+            }
+            count++;
+        }
+
+        this.displayPlayer(TemporalPlayerList);
+        Round.roundInit();
+        Round newRound = new Round();
+        newRound.translate(TemporalPlayerList);
+
+    }
+
+    private void swap(SinglyNode<TemporalPlayer> node1, SinglyNode<TemporalPlayer> node2) {
+        TemporalPlayer data1 = node1.getData();
+        TemporalPlayer data2 =  node2.getData();
+        node1.setData(data2);
+        node2.setData(data1);
     }
 
 
