a28b37af057d90c0847522300e8bce0aa22ac37e
JoeA42 <joe.alvarez42@gmail.com>
2020-05-27 19:17:44 -0600

    [Added Behaviour (w/ GabrielVL)] Absorbed behavior from the deprecated player class, extended entity so we can draw the player on screen before, added update method to change the stars and coins

--- src/com/itcr/ce/datosparty/logic/Player.java
+++ src/com/itcr/ce/datosparty/logic/Player.java
@@ -1,24 +1,47 @@
 package com.itcr.ce.datosparty.logic;
 
 import com.itcr.ce.datosparty.dataStructures.DoublyNode;
-import com.itcr.ce.datosparty.dataStructures.SinglyNode;
+import com.itcr.ce.datosparty.entities.Entity;
+import com.itcr.ce.datosparty.gfx.Assets;
 import com.itcr.ce.datosparty.logic.boxes.Box;
 
-public class Player{
+import java.awt.*;
+
+public class Player extends Entity {
 
     private final String name;
-    private int movement;
-    private int coins;
-    private int stars;
+
+    private int movement,
+                coins = 0,
+                stars = 0;
+
+    private float x;
+    private float y;
+
     private DoublyNode<Box> position;
 
-    public Player(String name) {
+    public Player(String name, float x, float y) {
+        super(x, y, 8, 8);
         this.name = name;
     }
 
-    public void move (SinglyNode<Box> box) {
-        //To move on board
+    public void move() {
+        int boxesLeft =  getMovement();
+        while (boxesLeft > 0) {
+            // Goes to next Node<Box>
+            setPosition(getPosition().getNext());
+            // Checks if there is a star
+            getPosition().getData().buyStar(this);
+            // Subtracts from number given on dice
+            boxesLeft--;
+            x = position.getData().getX();
+            y = position.getData().getY();
+            System.out.println("Position: " + (int) x + ", "+ (int) y );
+        }
+        update(this.getPosition().getData());
     }
+
+
 
     public String getName() {
         return name;
@@ -37,7 +60,7 @@ public class Player{
     }
 
     public void setCoins(int coins) {
-        this.coins = coins;
+        this.coins += coins;
     }
 
     public int getStars() {
@@ -54,5 +77,19 @@ public class Player{
 
     public void setPosition(DoublyNode<Box> position) {
         this.position = position;
+    }
+
+    public void update(Box currentBox) {
+        currentBox.boxAction(this);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.playerStatic,(int) x,(int) y, width, height, null);
     }
 }
