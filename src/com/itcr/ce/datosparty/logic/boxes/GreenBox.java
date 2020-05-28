6ffd4a0c5e096399837b46958cdedeb0c6d6e997
JoeA42 <joe.alvarez42@gmail.com>
2020-05-27 19:10:03 -0600

    [Specified boxAction behaviour(@GabrielVL)] Added box behaviour for green boxes they add coins to the Player.java

--- src/com/itcr/ce/datosparty/logic/boxes/GreenBox.java
+++ src/com/itcr/ce/datosparty/logic/boxes/GreenBox.java
@@ -1,6 +1,7 @@
 package com.itcr.ce.datosparty.logic.boxes;
 
 import com.itcr.ce.datosparty.gfx.Assets;
+import com.itcr.ce.datosparty.logic.Player;
 
 import java.awt.*;
 
@@ -18,5 +19,12 @@ public class GreenBox extends Box {
     public void render(Graphics g) {
         g.drawImage(Assets.greenBox,(int) x,(int) y, width, height, null);
 
+    }
+
+    @Override
+    public void boxAction(Player player){
+        player.setCoins(+3);
+        System.out.println("GreenBox");
+        System.out.println("Current Coins: "+player.getCoins());
     }
 }
