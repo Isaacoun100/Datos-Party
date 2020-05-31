7fe150cd79dc4f678f3b887d955c02ecc66c56cb
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 12:58:42 -0600

    [UI created] FourthMinigameUI is gonna handle FourthMinigameState.º

--- src/com/itcr/ce/datosparty/minigames/ui/FourthMinigameUI.java
+++ src/com/itcr/ce/datosparty/minigames/ui/FourthMinigameUI.java
@@ -1,4 +1,10 @@
 package com.itcr.ce.datosparty.minigames.ui;
 
-public class FourthMinigameUI {
+import com.itcr.ce.datosparty.Handler;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+
+public class FourthMinigameUI extends UIManager {
+    public FourthMinigameUI(Handler handler) {
+        super(handler);
+    }
 }
