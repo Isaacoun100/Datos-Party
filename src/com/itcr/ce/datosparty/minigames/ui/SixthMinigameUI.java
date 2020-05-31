f0ed0e7e5868fe4a0a6d52d330fbde860d121677
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 12:57:31 -0600

    [UI created] SixthMinigameUI is gonna handle SixthMinigameState.

--- src/com/itcr/ce/datosparty/minigames/ui/SixthMinigameUI.java
+++ src/com/itcr/ce/datosparty/minigames/ui/SixthMinigameUI.java
@@ -1,4 +1,10 @@
 package com.itcr.ce.datosparty.minigames.ui;
 
-public class SixthMinigameUI {
+import com.itcr.ce.datosparty.Handler;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+
+public class SixthMinigameUI extends UIManager {
+    public SixthMinigameUI(Handler handler) {
+        super(handler);
+    }
 }
