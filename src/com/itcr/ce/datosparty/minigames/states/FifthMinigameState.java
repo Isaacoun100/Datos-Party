b1e49b8911a262cd8052c48c8ba0cebcad2276ae
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 14:11:11 -0600

    [Blank Window state] A new window FifthMinigameState was created

--- src/com/itcr/ce/datosparty/minigames/states/FifthMinigameState.java
+++ src/com/itcr/ce/datosparty/minigames/states/FifthMinigameState.java
@@ -1,4 +1,28 @@
 package com.itcr.ce.datosparty.minigames.states;
 
-public class FifthMinigameState {
+import com.itcr.ce.datosparty.minigames.ui.FifthMinigameUI;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+import com.itcr.ce.datosparty.states.State;
+import com.itcr.ce.datosparty.Handler;
+
+import java.awt.*;
+
+public class FifthMinigameState extends State {
+    private UIManager uiManager;
+
+    public FifthMinigameState(Handler handler) {
+        super(handler);
+
+        uiManager = new FifthMinigameUI(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
