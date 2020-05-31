8e09dbc02578620fb76aee657f9f5b38a221f55c
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 14:11:49 -0600

    [Blank Window state] A new window EighthMinigameState was created

--- src/com/itcr/ce/datosparty/minigames/states/EighthMinigameState.java
+++ src/com/itcr/ce/datosparty/minigames/states/EighthMinigameState.java
@@ -1,4 +1,28 @@
 package com.itcr.ce.datosparty.minigames.states;
 
-public class EighthMinigameState {
+import com.itcr.ce.datosparty.minigames.ui.EighthMinigameUI;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+import com.itcr.ce.datosparty.states.State;
+import com.itcr.ce.datosparty.Handler;
+
+import java.awt.*;
+
+public class EighthMinigameState extends State {
+    private UIManager uiManager;
+
+    public EighthMinigameState(Handler handler) {
+        super(handler);
+
+        uiManager = new EighthMinigameUI(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
