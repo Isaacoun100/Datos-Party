c26793ee174d2f32834f20d6c88ab96729d5e6fe
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 14:08:42 -0600

    [Blank Window state] A new window SeventhMinigameState was created.

--- src/com/itcr/ce/datosparty/minigames/states/SeventhMinigameState.java
+++ src/com/itcr/ce/datosparty/minigames/states/SeventhMinigameState.java
@@ -1,4 +1,28 @@
 package com.itcr.ce.datosparty.minigames.states;
 
-public class SeventhMinigameState {
+import com.itcr.ce.datosparty.minigames.ui.SeventhMinigameUI;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+import com.itcr.ce.datosparty.states.State;
+import com.itcr.ce.datosparty.Handler;
+
+import java.awt.*;
+
+public class SeventhMinigameState extends State {
+    private UIManager uiManager;
+
+    public SeventhMinigameState(Handler handler) {
+        super(handler);
+
+        uiManager = new SeventhMinigameUI(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
