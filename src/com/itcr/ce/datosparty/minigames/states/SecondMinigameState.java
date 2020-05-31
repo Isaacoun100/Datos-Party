6f284a0d8435de271af44935f723c9169e77bc5d
Isaac <Isaacoun100@estudiantec.cr>
2020-05-31 14:09:09 -0600

    [Blank Window state] A new window SecondMinigameState was created

--- src/com/itcr/ce/datosparty/minigames/states/SecondMinigameState.java
+++ src/com/itcr/ce/datosparty/minigames/states/SecondMinigameState.java
@@ -1,4 +1,28 @@
 package com.itcr.ce.datosparty.minigames.states;
 
-public class SecondMinigameState {
+import com.itcr.ce.datosparty.minigames.ui.SecondMinigameUI;
+import com.itcr.ce.datosparty.userInterface.UIManager;
+import com.itcr.ce.datosparty.states.State;
+import com.itcr.ce.datosparty.Handler;
+
+import java.awt.*;
+
+public class SecondMinigameState extends State {
+    private UIManager uiManager;
+
+    public SecondMinigameState(Handler handler) {
+        super(handler);
+
+        uiManager = new SecondMinigameUI(handler);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
