a95865f68cb74a31ba45695e49b431e7fd9fb552
GabrielVL <gavalo.99@gmail.com>
2020-05-31 21:09:02 -0600

    [boardBuilder() method now adds boxes to phaseA ((w/ @JoeA42 & @Isaacoun100))] Also, Boxes are not added individually anymore.

--- src/com/itcr/ce/datosparty/logic/Board.java
+++ src/com/itcr/ce/datosparty/logic/Board.java
@@ -3,50 +3,53 @@ package com.itcr.ce.datosparty.logic;
 import com.itcr.ce.datosparty.dataStructures.CircularDoublyList;
 import com.itcr.ce.datosparty.entities.boxes.*;
 
-import java.util.Random;
-
 public class Board {
 
-    public static final CircularDoublyList<Box> phaseA = new CircularDoublyList<>();
+    public final CircularDoublyList<Box> phaseA = new CircularDoublyList<>();
+
+    public CircularDoublyList<Box> getPhaseA() {
+        return phaseA;
+    }
 
     public Board() {
 
-        phaseA.add(new BlueBox(400-(80*2)+80,30,80,80));
-        phaseA.add(new GreenBox(400-(80*2),30+80,80,80));
-        phaseA.add(new RedBox(400-(80*2),30+80*2,80,80));
-        phaseA.add(new YellowBox(400-(80*2)+80,30+80*3,80,80));
-        phaseA.add(new BlueBox(400,30+80,80,80));
-        phaseA.add(new GreenBox(400,30+80*2,80,80));
-        phaseA.add(new BlueBox(400-(80*2)+80,30,80,80));
-        phaseA.add(new GreenBox(400-(80*2),30+80,80,80));
-        phaseA.add(new RedBox(400-(80*2),30+80*2,80,80));
-        phaseA.add(new YellowBox(400-(80*2)+80,30+80*3,80,80));
-        phaseA.add(new BlueBox(400,30+80,80,80));
-        phaseA.add(new GreenBox(400,30+80*2,80,80));
-        phaseA.add(new BlueBox(400-(80*2)+80,30,80,80));
-        phaseA.add(new GreenBox(400-(80*2),30+80,80,80));
-        phaseA.add(new RedBox(400-(80*2),30+80*2,80,80));
-        phaseA.add(new YellowBox(400-(80*2)+80,30+80*3,80,80));
-        phaseA.add(new BlueBox(400,30+80,80,80));
-        phaseA.add(new GreenBox(400,30+80*2,80,80));
-        phaseA.add(new BlueBox(400-(80*2)+80,30,80,80));
-        phaseA.add(new GreenBox(400-(80*2),30+80,80,80));
-        phaseA.add(new RedBox(400-(80*2),30+80*2,80,80));
-        phaseA.add(new YellowBox(400-(80*2)+80,30+80*3,80,80));
-        phaseA.add(new BlueBox(400,30+80,80,80));
-        phaseA.add(new GreenBox(400,30+80*2,80,80));
-        phaseA.add(new BlueBox(400-(80*2)+80,30,80,80));
-        phaseA.add(new GreenBox(400-(80*2),30+80,80,80));
-        phaseA.add(new RedBox(400-(80*2),30+80*2,80,80));
-        phaseA.add(new YellowBox(400-(80*2)+80,30+80*3,80,80));
-        phaseA.add(new BlueBox(400,30+80,80,80));
-        phaseA.add(new GreenBox(400,30+80*2,80,80));
-
+        boardBuilder();
     }
 
-    public static void setStar() {
-        Random numRandom = new Random();
-        int starIndex = numRandom.nextInt(phaseA.getLength());
-        phaseA.getNodeByIndex(starIndex).getData().setStarBox(true);
+    public void boardBuilder(){
+
+        int xPos = 10;
+        int yPos = 10;
+        int boxColor;
+
+        for(int i = 0; i < 40; i++){
+            boxColor = Dice.roll(1, 4);
+            System.out.println(boxColor);
+            switch (Dice.roll(4, 1)){
+
+                case 1:
+                    phaseA.add(new BlueBox(xPos, yPos, 80, 80));
+                    break;
+                case 2:
+                    phaseA.add(new GreenBox(xPos, yPos, 80, 80));
+                    break;
+                case 3:
+                    phaseA.add(new RedBox(xPos, yPos, 80, 80));
+                    break;
+                case 4:
+                    phaseA.add(new YellowBox(xPos, yPos, 80, 80));
+                    break;
+
+                default:
+                    System.out.println("Something went terribly wrong");
+            }
+
+            if (i%5==0){
+                xPos = xPos + 80;
+                yPos = 10;
+            }
+            yPos = yPos + 80;
+        }
     }
+
 }
