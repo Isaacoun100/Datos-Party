02208a1aac2f07a2409807bdc76dbaef13828be9
GabrielVL <gavalo.99@gmail.com>
2020-05-31 21:41:40 -0600

    [Removed unnecessary methods on Round (w/ @JoeA42 & @Isaacoun100)] definePlayers() and returnPlayers() have been removed

--- src/com/itcr/ce/datosparty/logic/Round.java
+++ src/com/itcr/ce/datosparty/logic/Round.java
@@ -7,27 +7,13 @@ import com.itcr.ce.datosparty.entities.Player;
 public class Round {
 
     private static SinglyList<Player> playerOrder;
-    private static int numPlayers;
     private static int numRound = 0;
 
     public static void initRound() {
         playerOrder = new SinglyList<>();
     }
 
-    public static void definePlayers(SinglyList<String> playerNames) {
-        if (1 < playerNames.getLength() && playerNames.getLength() <= 4) {
-            numPlayers = playerNames.getLength();
-            playerOrder = new SinglyList<>();
-            for (int i = 0; i < 4; i++) {
-                playerOrder.add(new Player(playerNames.getNodeByIndex(i).getData(),0,0));
-                if (playerNames.getNodeByIndex(i).getNext() == null) {
-                    break;
-                }
-            }
-        }
-    }
-
-    public void translate(SinglyList<TemporalPlayer> sortedList){
+    public static void translate(SinglyList<TemporalPlayer> sortedList){
         SinglyNode<TemporalPlayer> temp = sortedList.getHead();
 
         while(temp!=null){
@@ -35,11 +21,11 @@ public class Round {
             temp=temp.getNext();
         }
 
-        this.showList();
+        showList();
 
     }
 
-    public void showList(){
+    public static void showList(){
 
         SinglyNode<Player> showPlayer = playerOrder.getHead();
 
@@ -52,10 +38,6 @@ public class Round {
 
     public static void addPlayer(String name){
         playerOrder.add(new Player(name,0,0));
-    }
-
-    public static void returnPlayers(){
-        playerOrder.print();
     }
 
     public static SinglyList<Player> getPlayerOrder() {
