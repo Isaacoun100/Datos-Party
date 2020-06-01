36b39a08e5638e2d3f4be43fe3c3714f6d7666e0
GabrielVL <gavalo.99@gmail.com>
2020-05-31 20:20:07 -0600

    [Star methods moved to Game (w/ @JoeA42 & @Isaacoun100)] Star methods have been removed from Box and Board classes

--- src/com/itcr/ce/datosparty/logic/Game.java
+++ src/com/itcr/ce/datosparty/logic/Game.java
@@ -51,4 +51,76 @@ public class Game extends Thread {
 
         }
     }
+
+    private void checkStar(Player player) {
+        Box playerBox = player.getPosition().getData();
+        if (playerBox.isStarBox()) {
+            buyStar(player);
+        }
+
+
+
+    }
+
+    public void setStar(){
+        Random numRandom = new Random();
+        CircularDoublyList<Box> phaseA =  handler.getBoard().getPhaseA();
+        int starIndex = numRandom.nextInt(phaseA.getLength());
+        Box starBox = phaseA.getNodeByIndex(starIndex).getData();
+        starBox.setStarBox(true);
+        
+    }
+
+    private void buyStar(Player player) {
+//        Scanner scanner = new Scanner(System.in);
+        System.out.println("Would you like to buy a star?");
+        // Choose yes/no
+        System.out.println("Are you sure? It's 10 coins.");
+        //Choose yes/no
+        if (player.getCoins() >= 10) {
+            player.setCoins(-10);
+            player.setStars(1);
+            player.getPosition().getData().setStarBox(false);
+            setStar();
+        } else {
+            System.out.println("You don't have enough money, though...");
+        }
+//        while (scanner.hasNext()) {
+//            if (scanner.next().equals("Yes")) {
+//                System.out.println("Are you sure? It's 10 coins.");
+//                // Choose yes/no
+//                if (scanner.next().equals("Yes")) {
+//                    if (player.getCoins() >= 10) {
+//                        player.setCoins(-10);
+//                        player.setStars(1);
+//                        this.starBox = false;
+//                        Board.setStar();
+//                    } else {
+//                        System.out.println("You don't have enough money, though...");
+//                    }
+//                } else if (scanner.next().equals("No")) {
+//                    System.out.println("Alright...");
+//                } else {
+//                    System.out.println("What? I'm gonna ask again...");
+//                    buyStar(player);
+//                }
+//            } else if (scanner.next().equals("No")) {
+//                System.out.println("Alright...");
+//            } else {
+//                System.out.println("What? I'm gonna ask again...");
+//                buyStar(player);
+//            }
+//        }
+//            if (scanner.next().equals("Yes"))
+//        System.out.println("Are you sure? It's 10 coins.");
+//        //Choose yes/no
+//        if (player.getCoins() >= 10) {
+//            player.setCoins(-10);
+//            player.setStars(1);
+//            this.starBox = false;
+//            Board.setStar();
+//        } else {
+//            System.out.println("You don't have enough money, though...");
+//        }
+    }
 }
