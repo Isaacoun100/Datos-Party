cf604920b16e878dc304e36a2e333d390ac50089
Isaac Herrera <Isaacoun100@gmail.com>
2020-05-31 21:02:14 -0600

    [Minigames implemented] Now, each mini game will take the player to a different state that will simulate the mechanics.

--- src/com/itcr/ce/datosparty/minigames/minilogic/Minigame.java
+++ src/com/itcr/ce/datosparty/minigames/minilogic/Minigame.java
@@ -15,28 +15,28 @@ public class Minigame {
 
         switch (gameID){
             case 1:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.firstMinigameState);
                 break;
             case 2:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.secondMinigameState);
                 break;
             case 3:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.thirdMinigameState);
                 break;
             case 4:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.fourthMinigameState);
                 break;
             case 5:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.fifthMinigameState);
                 break;
             case 6:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.sixthMinigameState);
                 break;
             case 7:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.seventhMinigameState);
                 break;
             case 8:
-                GameLoop.setState(GameLoop.creditsState);
+                GameLoop.setState(GameLoop.eighthMinigameState);
                 break;
             default:
                 System.out.println("A non existing minigame was invoke with the code  : "+gameID);
