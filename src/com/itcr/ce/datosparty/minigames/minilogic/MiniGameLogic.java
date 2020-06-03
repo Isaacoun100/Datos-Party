8a71480f98b5ee5cdc0030c8d2bd72028f0a3373
JoeA42 <joe.alvarez42@gmail.com>
2020-06-03 12:34:20 -0600

    [New Class] This is an abstract class for the MiniGameLogic for the different minigames

---
+++ src/com/itcr/ce/datosparty/minigames/minilogic/MiniGameLogic.java
@@ -0,0 +1,18 @@
+package com.itcr.ce.datosparty.minigames.minilogic;
+
+import com.itcr.ce.datosparty.dataStructures.SinglyList;
+import com.itcr.ce.datosparty.entities.Player;
+import com.itcr.ce.datosparty.logic.Game;
+
+public abstract class MiniGameLogic {
+
+    protected SinglyList<Player> playerList;
+    protected Game game;
+
+    public MiniGameLogic(Game game){
+        this.game = game;
+        this.playerList = game.getPlayerList();
+    }
+
+    public abstract void winGame(Player player);
+}
