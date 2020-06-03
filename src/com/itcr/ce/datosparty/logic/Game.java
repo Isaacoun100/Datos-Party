edd1eaaac804875eab7025b19ed4ec3994b10f19
JoeA42 <joe.alvarez42@gmail.com>
2020-06-03 12:28:49 -0600

    [Mayor rework @GabrielVL] we changed the way we instantiate some variables and we moved the round logic to the round class

--- src/com/itcr/ce/datosparty/logic/Game.java
+++ src/com/itcr/ce/datosparty/logic/Game.java
@@ -1,11 +1,14 @@
 package com.itcr.ce.datosparty.logic;
 
+import com.itcr.ce.datosparty.GameLoop;
 import com.itcr.ce.datosparty.Handler;
 import com.itcr.ce.datosparty.dataStructures.CircularDoublyList;
 import com.itcr.ce.datosparty.dataStructures.DoublyNode;
+import com.itcr.ce.datosparty.dataStructures.SinglyList;
 import com.itcr.ce.datosparty.entities.Player;
 import com.itcr.ce.datosparty.entities.boxes.Box;
-import com.itcr.ce.datosparty.states.PlayerSelectionState;
+import com.itcr.ce.datosparty.minigames.MiniGameBuilder;
+import com.itcr.ce.datosparty.minigames.minilogic.Minigame;
 
 import java.util.Random;
 
@@ -13,12 +16,18 @@ public class Game extends Thread {
 
     Handler handler;
     int currentRound = 1;
+    private SinglyList<Player> playerList;
+
+    public SinglyList<Player> getPlayerList() {
+        return playerList;
+    }
 
     public Game(Handler handler, int numRound) {
         this.handler = handler;
-        PlayerSelectionState pss = new PlayerSelectionState(handler);
-        Round.setNumRound(numRound);
-        Turn.setPlayersTurn(Round.getPlayerOrder().getHead());
+        Round.setMaxRound(numRound);
+        playerList = Round.getPlayerOrder();
+        Turn.setPlayersTurn(playerList.getHead());
+        int numberOfPlayers = Round.getPlayerOrder().getLength();
         Player currentPlayer;
         DoublyNode<Box> startBox = handler.getBoard().getPhaseA().getNodeByIndex(0);
         for(int i = 0; i < Round.getPlayerOrder().getLength(); i++){
@@ -27,54 +36,30 @@ public class Game extends Thread {
             currentPlayer.setPosition(startBox);
 
         }
+        MiniGameBuilder.build(GameLoop.miniGameStates,numberOfPlayers,handler, this);
+        System.out.println("number of players: "+Round.getPlayerOrder().getLength());
 
     }
 
     @Override
     public void run() {
-
-        while (currentRound <= Round.getNumRound()) {
-            Turn.setPlayersTurn(Round.getPlayerOrder().getHead());
-            Player currentPlayer;
-            if (currentRound == 2) {
-                System.out.println("Estrella");
-                setStar();
-            }
-            while (Turn.getPlayersTurn() != null) {
-                //Player
-                currentPlayer = Turn.getPlayersTurn().getData();
-                System.out.println(currentPlayer.getName());
-                Turn.rollDice();
-                Turn.movePlayer();
-                try {
-                    sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                checkStar(currentPlayer);
-                System.out.println("Dice: " + currentPlayer.getMovement());
-                System.out.print("\n");
-                Turn.nextPlayer();
+        while(currentRound != Round.getMaxRound()){
+            Round.playRound(this);
+            Minigame.playMinigame(1);
+            try {
+                pauseGame();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             currentRound++;
-
-//            try {
-//                this.wait();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-
         }
     }
 
-    private void checkStar(Player player) {
+    void checkStar(Player player) {
         Box playerBox = player.getPosition().getData();
         if (playerBox.isStarBox()) {
             buyStar(player);
         }
-
-
-
     }
 
     public void setStar(){
@@ -86,56 +71,31 @@ public class Game extends Thread {
         
     }
 
-    private void buyStar(Player player) {
+    public void buyStar(Player player) {
 //        Scanner scanner = new Scanner(System.in);
         System.out.println("Would you like to buy a star?");
         // Choose yes/no
         System.out.println("Are you sure? It's 10 coins.");
         //Choose yes/no
         if (player.getCoins() >= 10) {
-            player.setCoins(-10);
+            player.addCoins(-10);
             player.setStars(1);
             player.getPosition().getData().setStarBox(false);
             setStar();
         } else {
             System.out.println("You don't have enough money, though...");
         }
-//        while (scanner.hasNext()) {
-//            if (scanner.next().equals("Yes")) {
-//                System.out.println("Are you sure? It's 10 coins.");
-//                // Choose yes/no
-//                if (scanner.next().equals("Yes")) {
-//                    if (player.getCoins() >= 10) {
-//                        player.setCoins(-10);
-//                        player.setStars(1);
-//                        this.starBox = false;
-//                        Board.setStar();
-//                    } else {
-//                        System.out.println("You don't have enough money, though...");
-//                    }
-//                } else if (scanner.next().equals("No")) {
-//                    System.out.println("Alright...");
-//                } else {
-//                    System.out.println("What? I'm gonna ask again...");
-//                    buyStar(player);
-//                }
-//            } else if (scanner.next().equals("No")) {
-//                System.out.println("Alright...");
-//            } else {
-//                System.out.println("What? I'm gonna ask again...");
-//                buyStar(player);
-//            }
-//        }
-//            if (scanner.next().equals("Yes"))
-//        System.out.println("Are you sure? It's 10 coins.");
-//        //Choose yes/no
-//        if (player.getCoins() >= 10) {
-//            player.setCoins(-10);
-//            player.setStars(1);
-//            this.starBox = false;
-//            Board.setStar();
-//        } else {
-//            System.out.println("You don't have enough money, though...");
-//        }
+    }
+
+    public int getCurrentRound() {
+    return currentRound;
+    }
+
+    public synchronized void pauseGame() throws InterruptedException {
+        this.wait();
+    }
+
+    public synchronized void resumeGame() {
+        this.notify();
     }
 }
