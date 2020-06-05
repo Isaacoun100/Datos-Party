8312b510fe4d29d3321c32304cbd041e19db8c4b
JoeA42 <joe.alvarez42@gmail.com>
2020-06-05 01:09:14 -0600

    [Added Dice button] Dice button is now rendered in this state, when clicked it changes the movement of the player and resumes the game

--- src/com/itcr/ce/datosparty/states/GameState.java
+++ src/com/itcr/ce/datosparty/states/GameState.java
@@ -1,13 +1,20 @@
 package com.itcr.ce.datosparty.states;
 
 import com.itcr.ce.datosparty.GameLauncher;
+import com.itcr.ce.datosparty.GameLoop;
 import com.itcr.ce.datosparty.Handler;
-import com.itcr.ce.datosparty.dataStructures.*;
+import com.itcr.ce.datosparty.dataStructures.DoublyNode;
+import com.itcr.ce.datosparty.dataStructures.SinglyNode;
 import com.itcr.ce.datosparty.entities.Player;
 import com.itcr.ce.datosparty.entities.boxes.Box;
 import com.itcr.ce.datosparty.gfx.Assets;
+import com.itcr.ce.datosparty.logic.Game;
 import com.itcr.ce.datosparty.logic.Round;
+import com.itcr.ce.datosparty.logic.Turn;
+import com.itcr.ce.datosparty.music.MusicPlayer;
+import com.itcr.ce.datosparty.music.SoundEffect;
 import com.itcr.ce.datosparty.userInterface.GameUI;
+import com.itcr.ce.datosparty.userInterface.UIImageButton;
 import com.itcr.ce.datosparty.userInterface.UIManager;
 
 import java.awt.*;
@@ -18,21 +25,29 @@ public class GameState extends State{
 
     private int width = GameLauncher.width/16;
     private int height = GameLauncher.height/16;
+    private Player currentPlayer;
+    private Game game;
 
-    public GameState(Handler handler){
+    public GameState(Handler handler, Game game){
 
 
         super(handler);
 
         uiManager = new GameUI(handler);
 
-//        uiManager.addObject(new UIImageButton(20, 25, 3*3, 3, Assets.backButton,
-//                () -> State.setState(handler.getGameLoop().mainMenuState)));
+        uiManager.addObject(new UIImageButton(3, height-20, 8, 8, Assets.diceButton,
+                () -> {currentPlayer.setMovement();
+                    SoundEffect.DiceRoll();
+                    Thread.sleep(3000);
+                    game.resumeGame();
+                }));
+
 
     }
 
     @Override
     public void tick() {
+        currentPlayer = Turn.getPlayersTurn().getData();
         handler.getMouseManager().setUiManager(uiManager);
         uiManager.tick();
     }
@@ -40,6 +55,7 @@ public class GameState extends State{
     @Override
     public void render(Graphics g) {
         g.drawImage(Assets.mapGuide, -10, 0, null);
+
         uiManager.render(g);
 
         DoublyNode<Box> currentBoxMain = handler.getBoard().getMainCircuit().getHead();
