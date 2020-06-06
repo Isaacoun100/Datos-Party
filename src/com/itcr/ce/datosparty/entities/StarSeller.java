cbf2e08a680844f102e9d82ff84997885c6ead70
JoeA42 <joe.alvarez42@gmail.com>
2020-06-05 23:54:52 -0600

    [New Entity] Created new Entity Star seller to handle the positioning of the Star

---
+++ src/com/itcr/ce/datosparty/entities/StarSeller.java
@@ -0,0 +1,28 @@
+package com.itcr.ce.datosparty.entities;
+
+import com.itcr.ce.datosparty.gfx.Assets;
+
+import java.awt.*;
+
+public class StarSeller extends Entity{
+
+    public StarSeller(float x, float y) {
+        super(x, y, 150, 150);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.drawImage(Assets.starSeller,(int) x,(int) y, width, height, null);
+
+    }
+
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
