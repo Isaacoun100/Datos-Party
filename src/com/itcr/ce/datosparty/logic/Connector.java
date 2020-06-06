2ab73cde614cce14baaf9e45ca3a59ab25b79bee
JoeA42 <joe.alvarez42@gmail.com>
2020-06-05 23:46:34 -0600

    [Created Enum] Created enum for phase switching

---
+++ src/com/itcr/ce/datosparty/logic/Connector.java
@@ -0,0 +1,13 @@
+package com.itcr.ce.datosparty.logic;
+
+public enum Connector {
+    PHASE_A_FIRST,
+    PHASE_A_LAST,
+    PHASE_B_FIRST,
+    PHASE_B_LAST,
+    PHASE_C_FIRST,
+    PHASE_C_LAST,
+    PHASE_C_FIRST_REVERSED,
+    PHASE_C_LAST_REVERSED,
+    NONE
+}
