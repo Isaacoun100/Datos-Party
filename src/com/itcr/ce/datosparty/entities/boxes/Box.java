17b7df9ee9eaa7cc363c1ed347b5b936722b4b97
JoeA42 <joe.alvarez42@gmail.com>
2020-06-05 23:46:11 -0600

    [Added identifiers] Added variables for identifiers

--- src/com/itcr/ce/datosparty/entities/boxes/Box.java
+++ src/com/itcr/ce/datosparty/entities/boxes/Box.java
@@ -1,29 +1,43 @@
 package com.itcr.ce.datosparty.entities.boxes;
 
-import com.itcr.ce.datosparty.dataStructures.lists.SinglyList;
 import com.itcr.ce.datosparty.entities.Entity;
 import com.itcr.ce.datosparty.entities.Player;
 
 public abstract class Box extends Entity {
 
-    private SinglyList<Player> playerList = new SinglyList<>();
     private boolean starBox = false;
+
+    public boolean isCrossRoads() {
+        return crossRoads;
+    }
+
+    public void setCrossRoads(boolean crossRoads) {
+        this.crossRoads = crossRoads;
+    }
+
+    private boolean crossRoads = false;
+    private String boxID = "";
 
     public Box(float x, float y, int width, int height) {
         super(x, y, width, height);
-    }
-
-    public SinglyList<Player> getPlayerList() {
-        return playerList;
     }
 
     public boolean isStarBox() {
         return starBox;
     }
 
+    public String getBoxID() {
+        return boxID;
+    }
+
     public void setStarBox(boolean starBox) {
         this.starBox = starBox;
     }
+
+    public void setBoxID(String boxID) {
+        this.boxID = boxID;
+    }
+
 
     public abstract void boxAction(Player player);
 
