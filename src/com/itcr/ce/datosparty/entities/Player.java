ffbb72d776c3758d489f0f37df2ddcdaaad6a30f
JoeA42 <joe.alvarez42@gmail.com>
2020-06-05 23:52:19 -0600

    [Player now handles phase Switching] Player now handles phase switching using the enum and a boolean to determine if the switch is desired or not

--- src/com/itcr/ce/datosparty/entities/Player.java
+++ src/com/itcr/ce/datosparty/entities/Player.java
@@ -1,14 +1,15 @@
 package com.itcr.ce.datosparty.entities;
 
-import com.itcr.ce.datosparty.dataStructures.nodes.DoublyNode;
+import com.itcr.ce.datosparty.Handler;
 import com.itcr.ce.datosparty.dataStructures.nodes.Node;
-import com.itcr.ce.datosparty.gfx.Assets;
-import com.itcr.ce.datosparty.logic.Dice;
 import com.itcr.ce.datosparty.entities.boxes.Box;
+import com.itcr.ce.datosparty.logic.Connector;
 import com.itcr.ce.datosparty.logic.Game;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+
+import static com.itcr.ce.datosparty.logic.Connector.*;
 
 public class Player extends Entity {
 
@@ -21,6 +22,15 @@ public class Player extends Entity {
     private float x;
     private float y;
     BufferedImage image;
+    private Boolean changeDirection;
+
+    public Boolean getChangeDirection() {
+        return changeDirection;
+    }
+
+    public void setDirection(Boolean changeDirection) {
+        this.changeDirection = changeDirection;
+    }
 
     private Node<Box> position;
 
@@ -32,11 +42,48 @@ public class Player extends Entity {
         this.name = name;
     }
 
-    public void move(Game game) throws InterruptedException {
+    public void move(Game game, Handler handler) throws InterruptedException {
         int boxesLeft =  getMovement();
+        Node <Box> nextNode;
+        Node <Box> phaseAFirst = handler.getBoard().getPhaseA().getHead();
+        Node <Box> phaseALast = handler.getBoard().getMainCircuit().get(44);
+        Node <Box> phaseBFirst = handler.getBoard().getPhaseB().getHead();
+        Node <Box> phaseBLast = handler.getBoard().getMainCircuit().get(24);
+        Node <Box> phaseCFirst = handler.getBoard().getPhaseC().getHead();
+        Node <Box> phaseCLast = handler.getBoard().getMainCircuit().get(33);
         while (boxesLeft > 0) {
             // Goes to next Node<Box>
-            Node<Box> nextNode = getPosition().getNext();
+            if(getPosition().getData().isCrossRoads()){
+                game.pauseGame();
+            }
+            else {
+                setDirection(false);
+            }
+            switch (getConnector(getPosition(),handler, changeDirection)) {
+                case PHASE_A_FIRST -> {nextNode = phaseAFirst;
+                    setDirection(false);
+                }
+                case PHASE_A_LAST -> {nextNode = phaseALast;
+                    setDirection(false);
+                }
+                case PHASE_B_FIRST -> {nextNode = phaseBFirst;
+                    setDirection(false);
+                }
+                case PHASE_B_LAST -> {nextNode = phaseBLast;
+                    setDirection(false);
+                }
+                case PHASE_C_FIRST -> {nextNode = phaseCFirst;
+                    setDirection(false);
+                }
+                case PHASE_C_LAST -> {nextNode = phaseCLast;
+                    setDirection(false);
+                }
+                default -> {
+                    nextNode = getPosition().getNext();
+                    setDirection(false);
+                }
+            }
+          //  Node<Box> nextNode = getPosition().getNext();
             setPosition(nextNode);
             game.sleep(500);
             // Checks if there is a star
@@ -45,12 +92,59 @@ public class Player extends Entity {
             boxesLeft--;
             x = position.getData().getX();
             y = position.getData().getY() - 45;
-            System.out.println("Position: " + (int) x + ", "+ (int) y );
         }
         update(this.getPosition().getData());
     }
 
+    private Connector getConnector(Node<Box> current, Handler handler,boolean changeDirection) {
 
+        Node<Box> phaseAConnector = handler.getBoard().getMainCircuit().get(11);
+        Node<Box> phaseALast = handler.getBoard().getPhaseA().getLast();
+        Node<Box> phaseBConnector = handler.getBoard().getMainCircuit().get(15);
+        Node<Box> phaseBLast = handler.getBoard().getPhaseB().getLast();
+        Node<Box> phaseCConnector = handler.getBoard().getMainCircuit().get(12);
+        Node<Box> phaseCLast = handler.getBoard().getPhaseC().getLast();
+        Node<Box> phaseCConnectorReversed = handler.getBoard().getMainCircuit().get(33);
+        Node<Box> phaseCLastReversed = handler.getBoard().getPhaseC().getHead();
+
+        if(current == phaseAConnector){
+            if(changeDirection){
+                return PHASE_A_FIRST;
+            }
+            return NONE;
+        }
+        else if(current == phaseALast){
+                return PHASE_A_LAST;
+        }
+        else if(current == phaseBConnector){
+            if(changeDirection) {
+                return PHASE_B_FIRST;
+            }
+            return NONE;
+        }
+        else if(current == phaseBLast){
+            return PHASE_B_LAST;
+        }
+        else if(current == phaseCConnector){
+            if(changeDirection) {
+                return PHASE_C_FIRST;
+            }
+            return NONE;
+        }
+        else if(current == phaseCLast){
+            return PHASE_C_LAST;
+        }
+        else if(current == phaseCConnectorReversed){
+            if(changeDirection) {
+                return PHASE_C_FIRST_REVERSED;
+            }
+            return NONE;
+        }
+        else if(current == phaseCLastReversed){
+            return PHASE_C_LAST_REVERSED;
+        }
+        else return NONE;
+    }
 
     public String getName() {
         return name;
@@ -60,8 +154,8 @@ public class Player extends Entity {
         return movement;
     }
 
-    public void setMovement() {
-        this.movement = Dice.roll(6,1) + Dice.roll(6,1);
+    public void setMovement(int movement) {
+        this.movement = movement;
     }
 
     public int getCoins() {
