449b6b4be53c000970f5b51224f953ccb927c47c
JoeA42 <joe.alvarez42@gmail.com>
2020-06-07 16:05:03 -0600

    [UI updates] Added animation to Main Menu

--- src/com/itcr/ce/datosparty/states/MainMenuState.java
+++ src/com/itcr/ce/datosparty/states/MainMenuState.java
@@ -2,6 +2,8 @@ package com.itcr.ce.datosparty.states;
 
 import com.itcr.ce.datosparty.GameLauncher;
 import com.itcr.ce.datosparty.GameLoop;
+import com.itcr.ce.datosparty.gfx.Animation;
+import com.itcr.ce.datosparty.userInterface.UIAnimatedImage;
 import com.itcr.ce.datosparty.userInterface.UIImage;
 import com.itcr.ce.datosparty.userInterface.UIImageButton;
 import com.itcr.ce.datosparty.userInterface.UIManager;
@@ -16,12 +18,15 @@ public class MainMenuState extends State{
 
     private int width = GameLauncher.width/16;
     private int height = GameLauncher.height/16;
+    private Animation titleAnimation;
     public MainMenuState(Handler handler){
 
         super(handler);
 
+        titleAnimation = new Animation(500, Assets.titleImage);
+
         uiManager = new UIManager(handler);
-            uiManager.addObject(new UIImage((width/2)-19,height/6,40,6,Assets.titleImage));
+            uiManager.addObject(new UIAnimatedImage((width/2)-39,height/6,40*2,6*2,titleAnimation));
 
             uiManager.addObject(new UIImageButton((width/2)-4, (height/2), 9, 3, Assets.playButton,
                     () -> State.setState(GameLoop.selectPlayerState)));
@@ -36,6 +41,7 @@ public class MainMenuState extends State{
 
     @Override
     public void tick() {
+        //titleAnimation.tick();
         handler.getMouseManager().setUiManager(uiManager);
         uiManager.tick();
         }
@@ -43,6 +49,7 @@ public class MainMenuState extends State{
     @Override
     public void render(Graphics g) {
             uiManager.render(g);
+            //g.drawImage(titleAnimation.getCurrentFrame(), (width/2)-19, height/6,40,6,null);
         }
 
 }
