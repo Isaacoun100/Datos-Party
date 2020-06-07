710e709230a2c2aecaf25e8ffb67fdd65951a71d
JoeA42 <joe.alvarez42@gmail.com>
2020-06-07 16:11:02 -0600

    [Changed Parameters] UI image now only takes a single image

--- src/com/itcr/ce/datosparty/userInterface/UIImage.java
+++ src/com/itcr/ce/datosparty/userInterface/UIImage.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 
 public class UIImage extends UIObject{
 
-    private BufferedImage[] images;
+    private BufferedImage image;
     private ClickListener clicker;
 
     /**
@@ -14,25 +14,20 @@ public class UIImage extends UIObject{
      * @param y
      * @param width
      * @param height
-     * @param images this uses an Array List from java, remember to change it for one of the data structures Gabo created
+     * @param image
      */
-    public UIImage(float x, float y, int width, int height, BufferedImage[] images) {
+    public UIImage(float x, float y, int width, int height, BufferedImage image) {
         super(x*16, y*16, width*16, height*16);
-        this.images = images;
+        this.image = image;
     }
 
     @Override
     public void tick() {
-
-
     }
 
     @Override
     public void render(Graphics g) {
-        if(hovering)
-            g.drawImage(images[1], (int)x, (int)y, width, height, null);
-        else
-            g.drawImage(images[0], (int)x, (int)y, width, height, null);
+            g.drawImage(image, (int)x, (int)y, width, height, null);
     }
 
     @Override
