fa751fb79d56d6bec1184167a91859bde62c7653
JoeA42 <joe.alvarez42@gmail.com>
2020-06-07 19:26:11 -0600

    [Deleted Unnecessary Class] Deleted Class

--- src/com/itcr/ce/datosparty/userInterface/SelectRoundUI.java
+++
@@ -1,9 +0,0 @@
-package com.itcr.ce.datosparty.userInterface;
-
-import com.itcr.ce.datosparty.Handler;
-
-public class SelectRoundUI extends UIManager{
-    public SelectRoundUI(Handler handler) {
-        super(handler);
-    }
-}
