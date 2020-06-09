18a4abfaa947f977c05dc64e5a816073b1b62765
GabrielVL <gavalo.99@gmail.com>
2020-06-08 21:37:48 -0600

    [Method for most events moved] All methods now moved from YellowBox to EventLogic

--- src/com/itcr/ce/datosparty/logic/EventLogic.java
+++ src/com/itcr/ce/datosparty/logic/EventLogic.java
@@ -1,6 +1,13 @@
 package com.itcr.ce.datosparty.logic;
 
+import com.itcr.ce.datosparty.dataStructures.lists.CircularDoublyList;
+import com.itcr.ce.datosparty.dataStructures.lists.CircularList;
+import com.itcr.ce.datosparty.dataStructures.lists.DoublyList;
+import com.itcr.ce.datosparty.dataStructures.lists.SinglyList;
+import com.itcr.ce.datosparty.dataStructures.nodes.Node;
+import com.itcr.ce.datosparty.dataStructures.nodes.SinglyNode;
 import com.itcr.ce.datosparty.entities.Player;
+import com.itcr.ce.datosparty.entities.boxes.Box;
 
 public class EventLogic {
 
@@ -52,5 +59,77 @@ public class EventLogic {
             playerToAdd = (SinglyNode<Player>) playerToAdd.getNext();
         }
         System.out.println("Gifted " + randomCoins + " coins.");
+    }
+
+    public void loseStar(Player player, Game game) {
+        pauseEvent(game);
+        if (player.getStars() >= 1)  {
+            player.addStars(-1);
+            Player randomPlayer = returnRandomPlayer(player);
+            randomPlayer.addStars(1);
+            System.out.println("Stolen star from " + randomPlayer.getName());
+        }
+    }
+
+    public void winTwoStars(Player player, Game game) {
+        pauseEvent(game);
+        player.addStars(2);
+    }
+
+    public void winFiveStars(Player player, Game game) {
+        pauseEvent(game);
+        player.addStars(5);
+    }
+
+    public void stealStar(Player player, Game game) {
+        pauseEvent(game);
+        Player randomPlayer = returnRandomPlayer(player);
+        randomPlayer.addStars(-1);
+        if (randomPlayer.getStars() >= 1) {
+            player.addStars(1);
+        }
+        System.out.println("Stolen from " + randomPlayer.getName());
+    }
+
+    public void teleport(Player player, Game game) {
+        pauseEvent(game);
+        int numRandom;
+        CircularList<Box> mainCircuit =  game.getMainCircuit();
+        SinglyList<Box> phaseA = game.getPhaseA();
+        SinglyList<Box> phaseB = game.getPhaseB();
+        DoublyList<Box> phaseC = game.getPhaseC();
+        CircularDoublyList<Box> phaseD = game.getPhaseD();
+        switch (Dice.roll(1, 5)) {
+            case 1 -> {
+                numRandom = Dice.roll(0, mainCircuit.getLength() - 1);
+                player.setPosition(mainCircuit.get(numRandom));
+            }
+            case 2 -> {
+                numRandom = Dice.roll(0, phaseA.getLength() - 1);
+                player.setPosition(phaseA.get(numRandom));
+            }
+            case 3 -> {
+                numRandom = Dice.roll(0, phaseB.getLength() - 1);
+                player.setPosition(phaseB.get(numRandom));
+            }
+            case 4 -> {
+                numRandom = Dice.roll(0, phaseC.getLength() - 1);
+                player.setPosition(phaseC.get(numRandom));
+            }
+            case 5 -> {
+                numRandom = Dice.roll(0, phaseD.getLength() - 1);
+                player.setPosition(phaseD.get(numRandom));
+            }
+        }
+    }
+
+    public void swapPlayers(Player player, Game game) {
+        pauseEvent(game);
+        int numRandom = Dice.roll(0, Round.getPlayerOrder().getLength() - 1);
+        Player randomPlayer =  Round.getPlayerOrder().get(numRandom).getData();
+        Node<Box> randomPosition = randomPlayer.getPosition();
+        Node<Box> playerPosition = player.getPosition();
+        randomPlayer.setPosition(playerPosition);
+        player.setPosition(randomPosition);
     }
 }
