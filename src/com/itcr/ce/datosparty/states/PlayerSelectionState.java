0f53b1824eedd4e1ee5cb1383c41d130439af930
Isaac Herrera <Isaacoun100@gmail.com>
2020-06-10 20:12:19 -0600

    [New UIManager to differentiate the states] The old UIManager conflicted with the already existing UIManager

--- src/com/itcr/ce/datosparty/states/PlayerSelectionState.java
+++ src/com/itcr/ce/datosparty/states/PlayerSelectionState.java
@@ -5,10 +5,7 @@ import com.itcr.ce.datosparty.GameLoop;
 import com.itcr.ce.datosparty.gfx.Animation;
 import com.itcr.ce.datosparty.logic.Game;
 import com.itcr.ce.datosparty.logic.Leaderboard;
-import com.itcr.ce.datosparty.userInterface.UIAnimatedImage;
-import com.itcr.ce.datosparty.userInterface.UIImage;
-import com.itcr.ce.datosparty.userInterface.UIImageButton;
-import com.itcr.ce.datosparty.userInterface.UIManager;
+import com.itcr.ce.datosparty.userInterface.*;
 import com.itcr.ce.datosparty.Handler;
 import com.itcr.ce.datosparty.gfx.Assets;
 import com.itcr.ce.datosparty.logic.DefineOrder;
@@ -19,7 +16,7 @@ import java.awt.*;
 public class PlayerSelectionState extends State {
 
     private static Game game;
-    private final UIManager uiManager;
+    private final UIManager PlayerSelectionUI;
     DefineOrder setOrder = new DefineOrder();
     private int width = GameLauncher.width/16;
     private int height = GameLauncher.height/16;
@@ -31,16 +28,18 @@ public class PlayerSelectionState extends State {
     public PlayerSelectionState(Handler handler) {
         super(handler);
 
-        uiManager = new UIManager(handler);
+        PlayerSelectionUI = new UIManager(handler);
         DefineOrder.initTemporal();
+
+        PlayerSelectionUI.addObject(new UIBackground(Assets.selectionWallpaper, "wp"));
 
         Animation twoPlayerAnim = new Animation(600, Assets.twoPlayerAnim);
         Animation threePlayerAnim = new Animation(400, Assets.threePlayerAnim);
         Animation fourPlayerAnim = new Animation(200,Assets.fourPlayerAnim);
 
-        uiManager.addObject(new UIImage((float)width/2-40,(float)height/2-30,10*8,2*8,Assets.playerSelectionTitle,"SelectPlayerTitle"));
+        PlayerSelectionUI.addObject(new UIImage((float)width/2-40,(float)height/2-30,10*8,2*8,Assets.playerSelectionTitle,"SelectPlayerTitle"));
 
-        uiManager.addObject(new UIImageButton((float)width/2-44, (float)height/2+10, 7*4, 2*4, Assets.player2Button,"player2Btn", () -> {
+        PlayerSelectionUI.addObject(new UIImageButton((float)width/2-44, (float)height/2+10, 7*4, 2*4, Assets.player2Button,"player2Btn", () -> {
             setOrder.recursiveAdd(2);
             setOrder.order();
             Leaderboard.initLeaderBoard();
@@ -49,7 +48,7 @@ public class PlayerSelectionState extends State {
             State.setState(GameLoop.gameDependantStates.get(8).getData());
         }));
 
-        uiManager.addObject(new UIImageButton((float)width/2-14, (float)height/2+10, 7*4, 2*4, Assets.player3Button,"player3Btn", () -> {
+        PlayerSelectionUI.addObject(new UIImageButton((float)width/2-14, (float)height/2+10, 7*4, 2*4, Assets.player3Button,"player3Btn", () -> {
             setOrder.recursiveAdd(3);
             setOrder.order();
             Leaderboard.initLeaderBoard();
@@ -58,7 +57,7 @@ public class PlayerSelectionState extends State {
             State.setState(GameLoop.gameDependantStates.get(8).getData());
         }));
 
-        uiManager.addObject(new UIImageButton((float)width/2+16, (float)height/2+10, 7*4, 2*4, Assets.player4Button,"player4Btn", () -> {
+        PlayerSelectionUI.addObject(new UIImageButton((float)width/2+16, (float)height/2+10, 7*4, 2*4, Assets.player4Button,"player4Btn", () -> {
             setOrder.recursiveAdd(4);
             setOrder.order();
             Leaderboard.initLeaderBoard();
@@ -67,22 +66,22 @@ public class PlayerSelectionState extends State {
             State.setState(GameLoop.gameDependantStates.get(8).getData());
         }));
 
-        uiManager.addObject(new UIAnimatedImage((float)width/2-38,(float)height/2-6,2*8,2*8,twoPlayerAnim,"twoPlayerAnim"));
-        uiManager.addObject(new UIAnimatedImage((float)width/2-12,(float)height/2-6,3*8,2*8,threePlayerAnim,"threePlayerAnim"));
-        uiManager.addObject(new UIAnimatedImage((float)width/2+14,(float)height/2-6,4*8,2*8,fourPlayerAnim,"fourPlayerAnim"));
+        PlayerSelectionUI.addObject(new UIAnimatedImage((float)width/2-38,(float)height/2-6,2*8,2*8,twoPlayerAnim,"twoPlayerAnim"));
+        PlayerSelectionUI.addObject(new UIAnimatedImage((float)width/2-12,(float)height/2-6,3*8,2*8,threePlayerAnim,"threePlayerAnim"));
+        PlayerSelectionUI.addObject(new UIAnimatedImage((float)width/2+14,(float)height/2-6,4*8,2*8,fourPlayerAnim,"fourPlayerAnim"));
 
 
     }
 
     @Override
     public void tick() {
-        handler.getMouseManager().setUiManager(uiManager);
-        uiManager.tick();
+        handler.getMouseManager().setUiManager(PlayerSelectionUI);
+        PlayerSelectionUI.tick();
     }
 
     @Override
     public void render(Graphics g) {
-        uiManager.renderAll(g);
+        PlayerSelectionUI.renderAll(g);
     }
 
 }
