fe60dd844366bf1d4665e66d67b2e7d287245a6a
GabrielVL <gavalo.99@gmail.com>
2020-06-11 17:09:34 -0600

    [Enum with bomb directions] Created enum to move bomb easier

---
+++ src/com/itcr/ce/datosparty/minigames/miniGameLogic/BombDirections.java
@@ -0,0 +1,8 @@
+package com.itcr.ce.datosparty.minigames.miniGameLogic;
+
+public enum BombDirections {
+    TO_PLAYER_1,
+    TO_PLAYER_2,
+    TO_PLAYER_3,
+    TO_PLAYER_4,
+}
--- src/com/itcr/ce/datosparty/minigames/miniGameLogic/HotPotato.java
+++
@@ -1,7 +0,0 @@
-package com.itcr.ce.datosparty.minigames.miniGameLogic;
-
-public class HotPotato {
-
-
-
-}
