d70ab805086e50efd1d87dd5c81fd8214bd0a2c9
Isaac Herrera <Isaacoun100@gmail.com>
2020-06-14 21:55:33 -0600

    [Comments] The SelectRoundState is now properly commented

--- src/com/itcr/ce/datosparty/states/SelectRoundState.java
+++ src/com/itcr/ce/datosparty/states/SelectRoundState.java
@@ -7,15 +7,27 @@ import com.itcr.ce.datosparty.userInterface.*;
 
 import java.awt.*;
 
+/**
+ * In this method the players select the number of rounds they want to play
+ */
 public class SelectRoundState extends State {
 
     private final UIManager selectRoundUI;
     public static int round;
 
+    /**
+     * This method returns the number of rounds the players wanned to play once the player clicks on the button
+     * @return the number of rounds the players want to play
+     */
     public static int getRound() {
         return round;
     }
 
+    /**
+     * This is the constructor loads the buttons that asks the player how many rounds they want to play, it is
+     * initialized in the constructor because this state is called in the game just once.
+     * @param handler
+     */
     public SelectRoundState(Handler handler) {
         super(handler);
 
@@ -49,12 +61,21 @@ public class SelectRoundState extends State {
 
     }
 
+    /**
+     * The tick initializes once the state is called, the main difference with the constructor is that the constructor
+     * runs when the program itself starts but the tick each time the state is called
+     */
     @Override
     public void tick() {
         handler.getMouseManager().setUiManager(selectRoundUI);
         selectRoundUI.tick();
     }
 
+    /**
+     * Render method runs constantly in a loop once the game is started and finishes the moment the game also finishes,
+     * this method also is the method in charge of rendering the graphics
+     * @param g graphics parameter passed to GameLoop
+     */
     @Override
     public void render(Graphics g) {
         selectRoundUI.renderAll(g);
