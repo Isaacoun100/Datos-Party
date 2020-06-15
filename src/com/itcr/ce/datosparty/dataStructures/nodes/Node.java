c47095bfdd23a0fbdac8463824395d27cec11b82
GabrielVL <gavalo.99@gmail.com>
2020-06-14 23:44:40 -0600

    [Javadoc comments] Checked nodes' comments

--- src/com/itcr/ce/datosparty/dataStructures/nodes/Node.java
+++ src/com/itcr/ce/datosparty/dataStructures/nodes/Node.java
@@ -8,23 +8,51 @@ public  abstract class Node<T> {
 
     protected T data;
 
+    /**
+     * Constructor that sets the data to the node
+     * @param data data of generic type saved on new node
+     */
     public Node(T data) {
         this.data = data;
     }
 
+    /**
+     * Gets the data store on the node
+     * @return data stored on node
+     */
     public T getData() {
         return data;
     }
 
+    /**
+     * Sets the data stored on node
+     * @param data data stored on node
+     */
     public void setData(T data) {
         this.data = data;
     }
 
+    /**
+     * Gets the previous node on doubly nodes
+     * @return previous node (only on doubly nodes)
+     */
     public abstract Node<T> getPrevious();
 
+    /**
+     * Sets the previous reference on doubly nodes
+     * @param previous previous reference
+     */
     public abstract void setPrevious(Node<T> previous);
 
+    /**
+     * Gets the next node on both singly and doubly node
+     * @return next node
+     */
     public abstract Node<T> getNext();
 
+    /**
+     * Sets the next reference on both singly and doubly node
+     * @param next next reference
+     */
     public abstract void setNext(Node<T> next);
 }
--- src/com/itcr/ce/datosparty/dataStructures/nodes/SinglyNode.java
+++ src/com/itcr/ce/datosparty/dataStructures/nodes/SinglyNode.java
@@ -8,25 +8,47 @@ public  class SinglyNode<T> extends Node<T> {
 
     protected Node<T> next;
 
+    /**
+     * Constructor that sets the data to the node
+     * @param data data of generic type saved on new node
+     */
     public SinglyNode(T data) {
         super(data);
     }
 
+    /**
+     * As a singly node, it doesn't have a previous reference. This was made so casting could be implemented when it
+     * was uncertain which type of node would be set
+     * @return null
+     */
     @Override
     public Node<T> getPrevious() {
         System.out.println("SinglyNode doesn't have previous reference");
         return null;
     }
 
+    /**
+     * As a singly node, it doesn't have a previous reference. This was made so casting could be implemented when it
+     * was uncertain which type of node would be set
+     * @param previous doesn't set anything
+     */
     @Override
     public void setPrevious(Node<T> previous) {
         System.out.println("SinglyNode doesn't have previous reference");
     }
 
+    /**
+     * Gets the node that it references as its next. Used on doubly node.
+     * @return next node
+     */
     public Node<T> getNext() {
         return next;
     }
 
+    /**
+     * Sets a node and puts it after it, according to the list order. Used on doubly node.
+     * @param next next reference
+     */
     public void setNext(Node<T> next) {
         this.next = next;
     }
