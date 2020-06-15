d27ce1cc42751a2dcd3915c8956d60ddbc506429
GabrielVL <gavalo.99@gmail.com>
2020-06-15 01:22:18 -0600

    [Javadoc comments] Finished Turn comments and caught InterruptedException

--- src/com/itcr/ce/datosparty/logic/Turn.java
+++ src/com/itcr/ce/datosparty/logic/Turn.java
@@ -3,37 +3,68 @@ package com.itcr.ce.datosparty.logic;
 import com.itcr.ce.datosparty.dataStructures.nodes.SinglyNode;
 import com.itcr.ce.datosparty.entities.Player;
 
+/**
+ * Contains a node that indicates who is playing currently
+ */
 public class Turn {
 
     private static SinglyNode<Player> playersTurn;
 
+    /**
+     * Sets the turn to the next player
+     */
     public static void nextPlayer() {
         playersTurn = (SinglyNode<Player>) playersTurn.getNext();
     }
 
+    /**
+     * Gets the player playing currently
+     * @return player currently playing
+     */
     public static SinglyNode<Player> getPlayersTurn() {
         return playersTurn;
     }
 
-    public static void movePlayer(Game game) throws InterruptedException {
-        playersTurn.getData().move(game);
+    /**
+     * Moves player form box to box
+     * @param game game played currently
+     */
+    public static void movePlayer(Game game) {
+        try {
+            playersTurn.getData().move(game);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 
+    /**
+     * Sets the turn to the player currently playing
+     * @param playersTurn player currently playing
+     */
     public static void setPlayersTurn(SinglyNode<Player> playersTurn) {
         Turn.playersTurn = playersTurn;
     }
 
-    public static void playTurn(Game game, Player currentPlayer) throws InterruptedException {
-        while (Turn.getPlayersTurn() != null) {
-            boolean activeTurn = currentPlayer.getCurrentTurn();
-            while (activeTurn) {
-                System.out.println(currentPlayer.getName());
-                Turn.movePlayer(game);
-                game.pauseGame();
-                activeTurn = currentPlayer.getCurrentTurn();
-                System.out.println(activeTurn);
+    /**
+     * Combines methods to execute the turn correctly
+     * @param game game currently playing
+     * @param currentPlayer player currently playing
+     */
+    public static void playTurn(Game game, Player currentPlayer) {
+        try {
+            while (Turn.getPlayersTurn() != null) {
+                boolean activeTurn = currentPlayer.getCurrentTurn();
+                while (activeTurn) {
+                    System.out.println(currentPlayer.getName());
+                    Turn.movePlayer(game);
+                    game.pauseGame();
+                    activeTurn = currentPlayer.getCurrentTurn();
+                    System.out.println(activeTurn);
+                }
+                nextPlayer();
             }
-            nextPlayer();
+        } catch (InterruptedException e) {
+            System.out.println(e);
         }
     }
 }
